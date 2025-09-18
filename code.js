6913d7564223d61845ac31f22c8052ec669eab6e
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-09-18 16:21:35 +0000

    Console game works now need to add DOM

--- code.js
+++ code.js
@@ -2,11 +2,11 @@
 const Gameboard = (function () {
     let gameArray = ['' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ];
 
-    function add(position, symbol) {
-        if (gameArray[position] != '' ) {
+    function add(position, marker) {
+        if (gameArray[position] !== '' ) {
             return 1;
         } else {
-            gameArray[position] = symbol;
+            gameArray[position] = marker;
         }
     }
 
@@ -22,44 +22,44 @@ const Gameboard = (function () {
 })();
 
 const gameLogic = (function (gameArray) {
-    // Returns "X" if X wins and "O" if O wins, returns '' if no winner found.
-    function checkWinner() {
-        const winArray = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ];
-
-        let winnerFoundFlag = false;
-        for (let i = 0; i < 8; i++) {
-            let [a, b, c] = winArray[i];
-
-            if (gameArray[a] === gameArray[b] && gameArray[a] === gameArray[c]) {
-                winnerFoundFlag = true;
-                return gameArray[a];
-            }
-        }
-
-        if (!winnerFoundFlag) {
-            return '';
-        }
-    }
-
-    function checkTie(){
-        for (let i = 0; i < 9; i++) {
-            if (gameArray[i] === '') {
-                return false;
-            }
-        }
-        return true;
-    }
-
     function declareTheWin() {
+        // Returns "X" if X wins and "O" if O wins, returns '' if no winner found.
+        function checkWinner() {
+            const winArray = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6]
+            ];
+
+            let winnerFoundFlag = false;
+            for (let i = 0; i < 8; i++) {
+                let [a, b, c] = winArray[i];
+
+                if (gameArray[a] === gameArray[b] && gameArray[a] === gameArray[c]) {
+                    winnerFoundFlag = true;
+                    return gameArray[a];
+                }
+            }
+
+            if (!winnerFoundFlag) {
+                return '';
+            }
+        }
+
+        function checkTie(){
+            for (let i = 0; i < 9; i++) {
+                if (gameArray[i] === '') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
         const winner = checkWinner();
         if (winner === '') {
             const tie = checkTie();
@@ -72,5 +72,70 @@ const gameLogic = (function (gameArray) {
         return winner;
     }
 
-    return {declareTheWin};
+    function playGame() {
+        function chooseFirstMarker() {
+            let randomBinary = Math.floor(Math.random * 2);
+            if (randomBinary === 0) {
+                return "X";
+            } else {
+                return "O";
+            }
+        }
+        
+        function makeTurn(position, marker) {
+            if (Gameboard.add(position, marker) === 1) {
+                console.log("Incorrect Position: Try Again");
+                return "illegalMove";
+            }
+        }
+         
+        let currentMarker = chooseFirstMarker();
+        while(true) {
+            displayBoard(gameArray);
+            if (declareTheWin() === "O") {
+                console.log("O won");
+                Gameboard.resetBoard();
+                break;
+            } else if (declareTheWin() === "X") {
+                console.log("X won");
+                Gameboard.resetBoard();
+                break;
+            } else if (declareTheWin() === "tie") {
+                console.log("Tie!");
+                Gameboard.resetBoard();
+                break;
+            }
+            
+            let chosenPosition = parseInt(prompt(`Choose your position your marker is ${currentMarker}`));
+            if (Number.isNaN(chosenPosition)) {
+                console.log("Bad Position Argument");
+                break;
+            }
+
+            if (makeTurn(chosenPosition, currentMarker) === "illegalMove") {
+                continue;
+            }
+
+            if (currentMarker === "X") {
+                currentMarker = "O";
+            } else {
+                currentMarker = "X";
+            }
+        }
+    }
+
+    return {playGame};
 })(Gameboard.getGameArray());
+
+function displayBoard(gameArray) {
+    for (let i = 0; i < 9; i += 3) {
+        console.log(`${gameArray[i]} | ${gameArray[i+1]} | ${gameArray[i+2]}\n`);
+    }
+}
+
+function playerFactory(name) {
+    let playerName = name;
+    let playerMarker = "";
+
+    return {playerName, playerMarker};
+}
